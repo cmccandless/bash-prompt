a5853f2459d8d67c792ea6805cda3ab72e17548a
Corey McCandless <corey.mccandless@atlasied.com>
2018-10-22 12:43:39 -0400

    extract colors to ~/.config/ps1 (generate if missing)

--- set_prompt.sh
+++ set_prompt.sh
@@ -2,56 +2,66 @@
 # Custom bash prompt
 # Usage: add `source set_prompt.sh` to your .bashrc
 
-# Settings: 0 or 1
-DEBUG=0
-USE_16M_COLOR=1
+PS1_CONFIG_FILE="${HOME}/.config/ps1"
 
-# lowercase: decimal
-# uppercase: hexadecimal
-# rrr;ggg;bbb or #RGB or #RRGGBB or $VARIABLE
-COLOR_PREFIX="#fc0"
-COLOR_USER="#0c6"
-COLOR_HOST=$COLOR_USER
-COLOR_CWD="#077"
-COLOR_REPO="#0ff"
-# No changes to commit
-COLOR_BRANCH_CLEAN="#0f0"
-# All changes are staged
-COLOR_BRANCH_STAGED="#ff0"
-# Any modified or new files that are not staged
-COLOR_BRANCH_DIRTY="#f00"
-# Used for '$' at end of prompt
-COLOR_END="#fff"
-
-BOLD_USER=1
-BOLD_HOST=1
-BOLD_CWD=0
-BOLD_REPO=0
-BOLD_BRANCH=0
-BOLD_END=0
-
-unset PS1_SUFFIX
-
-dbg()
+__ps1_create_config()
 {
-    if [ $DEBUG -eq 1 ]; then
+    echo '#!/bin/bash'
+    echo
+    echo '# Settings: 0 or 1'
+    echo 'export PS1_DEBUG=0'
+    echo 'export PS1_USE_16M_COLOR=1'
+    echo
+    echo '# lowercase: decimal'
+    echo '# uppercase: hexadecimal'
+    echo '# rrr;ggg;bbb or #RGB or #RRGGBB or $VARIABLE'
+    echo 'export COLOR_PREFIX="#fc0"'
+    echo 'export COLOR_USER="#0c6"'
+    echo 'export COLOR_HOST=$COLOR_USER'
+    echo 'export COLOR_CWD="#077"'
+    echo 'export COLOR_REPO="#0ff"'
+    echo '# No changes to commit'
+    echo 'export COLOR_BRANCH_CLEAN="#0f0"'
+    echo '# All changes are staged'
+    echo 'export COLOR_BRANCH_STAGED="#ff0"'
+    echo '# Any modified or new files that are not staged'
+    echo 'export COLOR_BRANCH_DIRTY="#f00"'
+    echo '# Used for '$' at end of prompt'
+    echo 'export COLOR_END="#fff"'
+    echo
+    echo 'export BOLD_USER=1'
+    echo 'export BOLD_HOST=1'
+    echo 'export BOLD_CWD=0'
+    echo 'export BOLD_REPO=0'
+    echo 'export BOLD_BRANCH=0'
+    echo 'export BOLD_END=0'
+}
+
+if [ ! -f "$PS1_CONFIG_FILE" ]; then
+    __ps1_create_config > "$PS1_CONFIG_FILE"
+fi
+source "$PS1_CONFIG_FILE"
+
+__ps1_dbg()
+{
+    if [ $PS1_DEBUG -eq 1 ]; then
         >&2 echo $@
     fi
 }
 
-dec2hex()
+__ps1_dec2hex()
 {
     echo "obase=16; $1" | bc
 }
 
-octal_digit()
+__ps1_octal_digit()
 {
     x=$1
     y=7
     printf '%d' "$(( x>7?7:x ))"
 }
 
-color_from_hex(){
+__ps1_color_from_hex(){
     hex=${1#"#"}
     if [[ ( "${#hex}" == 3 ) ]]; then
         r=$(printf '0x%0.1s%0.1s' ${hex} ${hex})
@@ -62,53 +72,53 @@ color_from_hex(){
         g=$(printf '0x%0.2s' ${hex#??})
         b=$(printf '0x%0.2s' ${hex#????})
     fi
-    if [ $USE_16M_COLOR -eq 0 ]; then
+    if [ $PS1_USE_16M_COLOR -eq 0 ]; then
         index="$(( (r<75?0:(r-35)/40)*36 + 
                 (g<75?0:(g-35)/40)*6   +
                 (b<75?0:(b-35)/40)     + 16 ))"
         index=$(
-            for (( i=0; i<${#index}; i++)); do octal_digit ${index:$i:1}; done
+            for (( i=0; i<${#index}; i++)); do __ps1_octal_digit ${index:$i:1}; done
         )
     else
         index=$(printf "%03d;%03d;%03d" $r $g $b)
     fi
-    dbg "#$hex->$index"
+    __ps1_dbg "#$hex->$index"
     echo $index
 }
 
-reset_fmt(){
+__ps1_reset_fmt(){
     printf '\[\e[0m\]'
 }
 
-bold()
+__ps1_bold()
 {
-    printf "%s%s%s" '\[\e[1m\]' $1 $(reset_fmt)
+    printf "%s%s%s" '\[\e[1m\]' $1 $(__ps1_reset_fmt)
 }
 
-color_start()
+__ps1_color_start()
 {
     case "$1" in
-        "#"*) COLOR=$(color_from_hex "$1");;
+        "#"*) COLOR=$(__ps1_color_from_hex "$1");;
         *";"*";"*)  COLOR="$1"::
     esac
-    if [ $USE_16M_COLOR -eq 0 ]; then
+    if [ $PS1_USE_16M_COLOR -eq 0 ]; then
         printf '\[\e[38;5;%dm\]' "$COLOR"
     else
         printf '\[\e[038;2;%sm\]' "$COLOR"
     fi
 }
 
-color(){
-    COLOR="$(color_start "$1")"
+__ps1_color(){
+    COLOR="$(__ps1_color_start "$1")"
     TEXT=$2
-    if [ $USE_16M_COLOR -eq 0 ]; then
-        printf '%s%s%s' $COLOR "$TEXT" $(reset_fmt)
+    if [ $PS1_USE_16M_COLOR -eq 0 ]; then
+        printf '%s%s%s' $COLOR "$TEXT" $(__ps1_reset_fmt)
     else
-        printf '%s%s%s' $COLOR "$TEXT" $(reset_fmt)
+        printf '%s%s%s' $COLOR "$TEXT" $(__ps1_reset_fmt)
     fi
 }
 
-git_repo()
+__ps1_git_repo()
 {
     REPO=$(git config --get remote.origin.url || git rev-parse --show-toplevel 2> /dev/null)
     if [ $? -eq 0 ]; then
@@ -116,14 +126,14 @@ git_repo()
     fi
 }
 
-git_branch()
+__ps1_git_branch()
 {
-    REPO="$(git_repo)"
+    REPO="$(__ps1_git_repo)"
     BRANCH="$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
     if [[ "$REPO" ]]; then
-        REPO="$(color $COLOR_REPO $REPO)"
+        REPO="$(__ps1_color $COLOR_REPO $REPO)"
         if [ $BOLD_REPO -eq 1 ]; then
-            REPO="$(bold $REPO)"
+            REPO="$(__ps1_bold $REPO)"
         fi
         if [ -z "$BRANCH" ]; then
             BRANCH="$(git status 2> /dev/null | grep -oP '(?<=On branch ).*')"
@@ -135,37 +145,37 @@ git_branch()
     fi
 }
 
-parse_git_info()
+__ps1_git_info()
 {
-    REPO="$(git_repo)"
-    BRANCH="$(git_branch)"
+    REPO="$(__ps1_git_repo)"
+    BRANCH="$(__ps1_git_branch)"
     if [[ "$BRANCH" ]]; then
-        REPO="$(color $COLOR_REPO $REPO)"
+        REPO="$(__ps1_color $COLOR_REPO $REPO)"
         if [ $BOLD_REPO -eq 1 ]; then
-            REPO="$(bold $REPO)"
+            REPO="$(__ps1_bold $REPO)"
         fi
         if git status | grep -E 'Changes not staged|Untracked files|Unmerged paths' > /dev/null; then
-            BRANCH="$(color $COLOR_BRANCH_DIRTY "$BRANCH*")"
+            BRANCH="$(__ps1_color $COLOR_BRANCH_DIRTY "$BRANCH*")"
         elif git status | grep 'Changes to be committed' > /dev/null; then
-            BRANCH=$(color $COLOR_BRANCH_STAGED "$BRANCH*")
+            BRANCH=$(__ps1_color $COLOR_BRANCH_STAGED "$BRANCH*")
         else
-            BRANCH="$(color $COLOR_BRANCH_CLEAN $BRANCH)"
+            BRANCH="$(__ps1_color $COLOR_BRANCH_CLEAN $BRANCH)"
         fi
         if [ $BOLD_BRANCH -eq 1 ]; then
-            GIT_INFO+="$(bold $BRANCH)"
+            GIT_INFO+="$(__ps1_bold $BRANCH)"
         fi
         echo "($REPO: $BRANCH)\n"
     fi
 }
 
-join_by()
+__ps1_join_by()
 {
     local IFS="$1"
     shift
     echo "$*"
 }
 
-trim() {
+__ps1_trim() {
     local var="$*"
     # remove leading whitespace characters
     var="${var#"${var%%[![:space:]]*}"}"
@@ -174,7 +184,7 @@ trim() {
     echo -n "$var"
 }
 
-build_prefix()
+__ps1_build_prefix()
 {
     pref=()
     if [ ! -z "${VIRTUAL_ENV+x}" ]; then
@@ -183,21 +193,21 @@ build_prefix()
     if [ ! -z "${VIRTUAL_ENV_TOOLS+x}" ]; then
         pref+=("${ENV_NAME_TOOLS}-tools")
     fi
-    pref="$(trim "$(join_by '; ' ${pref[@]})")"
+    pref="$(__ps1_trim "$(__ps1_join_by '; ' ${pref[@]})")"
     echo "${pref}"
 }
 
-set_ps1()
+__ps1_set()
 {
     PS1=""
-    PS1_PREFIX="$(build_prefix)"
+    PS1_PREFIX="$(__ps1_build_prefix)"
     # if [ ! -z "${PS1_PREFIX+x}" ]; then
     if [ ! -z "${PS1_PREFIX}" ]; then
         PS1_PREFIX="(${PS1_PREFIX})"
-        PS1+="$(color $COLOR_PREFIX "$PS1_PREFIX ")"
+        PS1+="$(__ps1_color $COLOR_PREFIX "$PS1_PREFIX ")"
     fi
 
-    git_info="$(parse_git_info)"
+    git_info="$(__ps1_git_info)"
     if [ ! -z "$git_info" ]; then
         if [ ! -z "$PS1" ]; then
             PS1+=' '
@@ -207,33 +217,32 @@ set_ps1()
 
     PS1+="${debian_chroot:+($debian_chroot)}"
     if [ $BOLD_USER -eq 1 ]; then
-        PS1+="$(bold $(color $COLOR_USER \\u))"
+        PS1+="$(__ps1_bold $(__ps1_color $COLOR_USER \\u))"
     else
-        PS1+="$(color $COLOR_USER \\u)"
+        PS1+="$(__ps1_color $COLOR_USER \\u)"
     fi
     PS1+="@"
     if [ $BOLD_HOST -eq 1 ]; then
-        PS1+="$(bold $(color $COLOR_HOST \\h))"
+        PS1+="$(__ps1_bold $(__ps1_color $COLOR_HOST \\h))"
     else
-        PS1+="$(color $COLOR_HOST \\h)"
+        PS1+="$(__ps1_color $COLOR_HOST \\h)"
     fi
     PS1+=":"
     if [ $BOLD_CWD -eq 1 ]; then
-        PS1+="$(bold $(color $COLOR_CWD \\w))"
+        PS1+="$(__ps1_bold $(__ps1_color $COLOR_CWD \\w))"
     else
-        PS1+="$(color $COLOR_CWD \\w)"
+        PS1+="$(__ps1_color $COLOR_CWD \\w)"
     fi
     if [ $BOLD_END -eq 1 ]; then
-        PS1+="$(bold $(color $COLOR_END \\$))"
+        PS1+="$(__ps1_bold $(__ps1_color $COLOR_END \\$))"
     else
-        PS1+="$(color $COLOR_END \\$)"
+        PS1+="$(__ps1_color $COLOR_END \\$)"
     fi
     PS1+=" "
 
     export PS1
-    export PS1_SUFFIX=${PS1//$PS1_PREFIX /}
 }
 
-if [[ ! "$PROMPT_COMMAND" = *"set_ps1"* ]]; then
-    PROMPT_COMMAND+='set_ps1;'
+if [[ ! "$PROMPT_COMMAND" = *"__ps1_set"* ]]; then
+    PROMPT_COMMAND+='__ps1_set;'
 fi
